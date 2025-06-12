751edbce57280b3750e37e0612324270f3769d33
haywhyyhwyah <ayomideisrael623@gmail.com>
2025-06-12 09:32:25 +0100

    againnnnnnn

--- script.js
+++ script.js
@@ -12,30 +12,34 @@ let songGradeB = new Audio("Audio/Good.wav")
 let songGradeA = new Audio("Audio/Excellence.wav")
 
 function checkGrade() {
-    if (scoreInput.value >= 0 && scoreInput.value < 40) {
-        show.innerHTML = 'F - Fail💀'
-        songFail.play()
-    } else if (scoreInput.value >= 40 && scoreInput.value < 45) {
-        show.innerHTML = 'E - Pass😏'
-        songGradeE.play()
-    } else if (scoreInput.value >= 45 && scoreInput.value < 50) {
-        show.innerHTML = 'D - Average🙄'
-        songGradeD.play()
-    }else if (scoreInput.value >= 50 && scoreInput.value < 60) {
-        show.innerHTML = 'C - Good😉'
-        songGradeC.play()
-    }else if (scoreInput.value >= 60 && scoreInput.value < 75) {
-        show.innerHTML = 'B - Very Good😊'
-        songGradeB.play()
-    }else if (scoreInput.value >= 75 && scoreInput.value <=100) {
-        show.innerHTML = 'A - Excellent👍'
-        songGradeA.play()
-    }else if (scoreInput.value > 100) {
-        show.innerHTML = 'Invalid input❗'
-    }else if (scoreInput.value < 0) {
-        show.innerHTML = 'Invalid input❗'
-    }else if ( scoreInput.value === '') {
+    if (scoreInput.value === '') {
         show.innerHTML = 'Enter a Score'
+    } else {
+        if (scoreInput.value >= 0 && scoreInput.value < 40) {
+            show.innerHTML = 'F - Fail💀'
+            songFail.play()
+        } else if (scoreInput.value >= 40 && scoreInput.value < 45) {
+            show.innerHTML = 'E - Pass😏'
+            songGradeE.play()
+        } else if (scoreInput.value >= 45 && scoreInput.value < 50) {
+            show.innerHTML = 'D - Average🙄'
+            songGradeD.play()
+        }else if (scoreInput.value >= 50 && scoreInput.value < 60) {
+            show.innerHTML = 'C - Good😉'
+            songGradeC.play()
+        }else if (scoreInput.value >= 60 && scoreInput.value < 75) {
+            show.innerHTML = 'B - Very Good😊'
+            songGradeB.play()
+        }else if (scoreInput.value >= 75 && scoreInput.value <=100) {
+            show.innerHTML = 'A - Excellent👍'
+            songGradeA.play()
+        }else if (scoreInput.value > 100) {
+            show.innerHTML = 'Invalid input❗'
+        }else if (scoreInput.value < 0) {
+            show.innerHTML = 'Invalid input❗'
+        }else if ( scoreInput.value === '') {
+            show.innerHTML = 'Enter a Score'
+        }
     }
 }
 
